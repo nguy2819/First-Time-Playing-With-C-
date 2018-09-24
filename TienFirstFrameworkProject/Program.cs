cb77ca985c0cdacfb0b925200a698b5cc248b5b6
nguy2819 <borlandtien@gmail.com>
2018-09-24 17:49:44 -0600

    Learning more about If Statement
    boolean, else if, !=, ==, &&, >=, <=, >, <. Next will learn about
    Building a Better Calculator

--- TienFirstFrameworkProject/Program.cs
+++ TienFirstFrameworkProject/Program.cs
@@ -143,10 +143,68 @@ namespace TienFirstFrameworkProject
             Console.WriteLine(Cube(5)); //Return Statement - 125 
 
             int cubedNumber = Cube(5);
-            Console.WriteLine(cubedNumber);
+            Console.WriteLine(cubedNumber); //Return Statement -125
+
+
+
+
+
+            //If Statement
+            bool isMale = false;
+            bool isTall = true;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            } else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            } else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are either not male and not tall");
+            }
+
+            if (isMale || isTall)
+            {
+                Console.WriteLine("You are a male or you are a tall person, we are just a if - or guessing statement");
+            }
+            else
+            {
+                Console.WriteLine("You are either not a male or a tall person");
+            }
+
+
+
+            //More If Statement
+            Console.WriteLine(GetMax(20, 10, 4));
 
             Console.ReadLine(); //pause the screen so we can see the result in a longer time
         }
+
+        //More If Statement
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result1;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result1 = num1;
+            }else if (num2 >= num1 && num2 >= num3)
+            {
+                result1 = num2;
+            }
+            else
+            {
+                result1 = num3;
+            }
+            return result1;
+        }
+
+
+
 
         //Methods
         static void SayHi(string name)
